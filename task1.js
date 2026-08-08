3ea2af95404120ed3d899f3a570ae4901e5bf1f2
muruganthirupathi25-ops <muruganthirupathi25@gmail.com>
2026-08-08 20:31:07 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,464 @@
+
+// 1. create variable using
+
+const collegeName = "TECH SOFTWARE COLLAGE";
+let studentName = "AIJTH KUMER";
+var studentAge = 28;
+console.log(collegeName);
+console.log(studentName);
+console.log(studentAge);
+
+
+// task2 -mobile price
+
+let price = 25000;
+price = 27000;
+console.log(price);
+
+// task3 company name
+
+
+const company = "log company";
+company = "weak company";
+console.log(company);
+
+// task4 console practice
+
+console.log("Login Successful");
+console.warn("Password is Weak");
+console.error("Network Error");
+
+
+// task5 datatype
+
+let name = "AK";
+let age = 32;
+let isDeveloper = true;
+let data;
+console.log(name, typeof name);
+console.log(age, typeof age);
+console.log(isDeveloper, typeof isDeveloper);
+console.log(data, typeof data);
+
+// task6 Shopping cart array
+
+let cart = [
+    "Laptop",
+    "Mouse",
+    "Keyboard",
+    "Headset",
+    "Monitor"
+];
+console.log("First product:", cart[0]);
+console.log("Third product:", cart[2]);
+console.log("Last product:", cart[cart.length - 1]);
+
+// task7 favorite movies
+
+let movies = [
+    "Leo",
+    "Jailer",
+    "Vikram",
+    "Master",
+    "Ghilli",
+    "Kaithi"
+]
+console.log("2nd movie:", movies[1]);
+console.log("5th movie:", movies[4]);
+console.log("Last movie:", movies[movies.length - 1]);
+
+// task8 employee object
+let employee = {
+    name: "Thirupathi",
+    age: 22,
+    department: "IT",
+    skills: ["JavaScript", "Java", "HTML"],
+    salary: 30000
+};
+console.log("Employee Name:", employee.name);
+console.log("Department:", employee.department);
+console.log("First Skill:", employee.skills[0]);
+console.log("Salary:", employee.salary);
+
+// task9 product details
+
+
+let product = {
+    productName: "Mobile Phone",
+    brand: "vivo",
+    price: 27000,
+    colors: ["Black", "Blue", "White"]
+};
+console.log("Brand:", product.brand);
+console.log("Price:", product.price);
+console.log("Second Color:", product.colors[1]);
+
+// task10 arithmetic operater
+
+let a1 = 20;
+let b2 = 5;
+console.log("Addition:", a1 + b2);
+console.log("Subtraction:", a1 - b2);
+console.log("Multiplication:", a1 * b2);
+console.log("Division:", a1 / b2);
+console.log("Modulus:", a1 % b2);
+console.log("Exponent:", a1 ** b2);
+
+// task11 increment practice
+
+let a = 5;
+a++;
+console.log(a);
+++a;
+console.log(a);
+a--;
+console.log(a);
+--a;
+console.log(a);
+
+// task12 predict the output
+
+let a = 10;
+let b = a++;
+let c = ++b;
+ console.log(a);  11
+console.log(b);   12
+console.log(c);  12
+
+
+// task13 predict the output
+let x = 7;
+let y = --x;
+let z = y++;
+console.log(x); 6 
+console.log(y); 7
+console.log(z); 6
+
+// task 14 school object
+
+
+let school = {
+    schoolName: "ABC Higher Secondary School",
+    principal: "Mr. Kumar",
+    totalStudents: 1200,
+    classes: ["10th", "11th", "12th"]
+};
+console.log("School Name:", school.schoolName);
+console.log("Principal:", school.principal);
+console.log("Last Class:", school.classes[school.classes.length - 1]);
+
+
+// task15 grocery lit
+
+let groceries = [
+    "Rice",
+    "Milk",
+    "Sugar",
+    "Bread",
+    "Eggs",
+    "Oil",
+    "Salt",
+    "Tomato"
+];
+
+console.log("First Item:", groceries[0]);
+console.log("Fourth Item:", groceries[3]);
+console.log("Last Item:", groceries[groceries.length - 1]);
+
+
+// task16 user porfile
+
+let username = "Thirupathi";
+let email = "thirupathi@gmail.com";
+let age = 22;
+let isPremium = true;
+
+console.log(username, typeof username);
+console.log(email, typeof email);
+console.log(age, typeof age);
+console.log(isPremium, typeof isPremium);
+
+
+// task17 variable rules
+
+
+var a = 10;
+a = 20;       // Allowed
+var a = 30;   // Allowed
+
+
+let b = 10;
+b = 20;       // Allowed
+ let b = 30;   // Error
+
+const c = 10;
+ c = 20;       // Error
+ const c = 30; // Error
+
+
+//  task18 company database 
+
+let tamil = 85;
+let english = 90;
+let maths = 95;
+let total = tamil + english + maths;
+let average = total / 3;
+console.log("Total:", total);
+console.log("Average:", average);
+
+// task19 company database
+
+let company = {
+    Company: "LOG COMPANY",
+    Employees: ["Thirupathi", "Arun", "Kumar"],
+    Location: "Chennai",
+    FoundedYear: 2020,
+    CEO: "AIJTH KUMAR"
+};
+console.log("CEO:", company.CEO);
+console.log("Location:", company.Location);
+console.log("First Employee:", company.Employees[0]);
+
+// task20 mini profile
+
+let profile = {
+    name: "Thirupathi",
+    city: "Chennai",
+    hobbies: ["Coding", "Cricket", "Music"],
+    age: 22
+};
+console.log("Name:", profile.name);
+console.log("Second Hobby:", profile.hobbies[1]);
+console.log("Age:", profile.age);
+
+// 1. create variable using
+
+const collegeName = "TECH SOFTWARE COLLAGE";
+let studentName = "AIJTH KUMER";
+var studentAge = 28;
+console.log(collegeName);
+console.log(studentName);
+console.log(studentAge);
+
+
+// task2 -mobile price
+
+let price = 25000;
+price = 27000;
+console.log(price);
+
+// task3 company name
+
+
+const company = "log company";
+company = "weak company";
+console.log(company);
+
+// task4 console practice
+
+console.log("Login Successful");
+console.warn("Password is Weak");
+console.error("Network Error");
+
+
+// task5 datatype
+
+let name = "AK";
+let age = 32;
+let isDeveloper = true;
+let data;
+console.log(name, typeof name);
+console.log(age, typeof age);
+console.log(isDeveloper, typeof isDeveloper);
+console.log(data, typeof data);
+
+// task6 Shopping cart array
+
+let cart = [
+    "Laptop",
+    "Mouse",
+    "Keyboard",
+    "Headset",
+    "Monitor"
+];
+console.log("First product:", cart[0]);
+console.log("Third product:", cart[2]);
+console.log("Last product:", cart[cart.length - 1]);
+
+// task7 favorite movies
+
+let movies = [
+    "Leo",
+    "Jailer",
+    "Vikram",
+    "Master",
+    "Ghilli",
+    "Kaithi"
+]
+console.log("2nd movie:", movies[1]);
+console.log("5th movie:", movies[4]);
+console.log("Last movie:", movies[movies.length - 1]);
+
+// task8 employee object
+let employee = {
+    name: "Thirupathi",
+    age: 22,
+    department: "IT",
+    skills: ["JavaScript", "Java", "HTML"],
+    salary: 30000
+};
+console.log("Employee Name:", employee.name);
+console.log("Department:", employee.department);
+console.log("First Skill:", employee.skills[0]);
+console.log("Salary:", employee.salary);
+
+// task9 product details
+
+
+let product = {
+    productName: "Mobile Phone",
+    brand: "vivo",
+    price: 27000,
+    colors: ["Black", "Blue", "White"]
+};
+console.log("Brand:", product.brand);
+console.log("Price:", product.price);
+console.log("Second Color:", product.colors[1]);
+
+// task10 arithmetic operater
+
+let a1 = 20;
+let b2 = 5;
+console.log("Addition:", a1 + b2);
+console.log("Subtraction:", a1 - b2);
+console.log("Multiplication:", a1 * b2);
+console.log("Division:", a1 / b2);
+console.log("Modulus:", a1 % b2);
+console.log("Exponent:", a1 ** b2);
+
+// task11 increment practice
+
+let a = 5;
+a++;
+console.log(a);
+++a;
+console.log(a);
+a--;
+console.log(a);
+--a;
+console.log(a);
+
+// task12 predict the output
+
+let a = 10;
+let b = a++;
+let c = ++b;
+ console.log(a);  11
+console.log(b);   12
+console.log(c);  12
+
+
+// task13 predict the output
+let x = 7;
+let y = --x;
+let z = y++;
+console.log(x); 6 
+console.log(y); 7
+console.log(z); 6
+
+// task 14 school object
+
+
+let school = {
+    schoolName: "ABC Higher Secondary School",
+    principal: "Mr. Kumar",
+    totalStudents: 1200,
+    classes: ["10th", "11th", "12th"]
+};
+console.log("School Name:", school.schoolName);
+console.log("Principal:", school.principal);
+console.log("Last Class:", school.classes[school.classes.length - 1]);
+
+
+// task15 grocery lit
+
+let groceries = [
+    "Rice",
+    "Milk",
+    "Sugar",
+    "Bread",
+    "Eggs",
+    "Oil",
+    "Salt",
+    "Tomato"
+];
+
+console.log("First Item:", groceries[0]);
+console.log("Fourth Item:", groceries[3]);
+console.log("Last Item:", groceries[groceries.length - 1]);
+
+
+// task16 user porfile
+
+let username = "Thirupathi";
+let email = "thirupathi@gmail.com";
+let age = 22;
+let isPremium = true;
+
+console.log(username, typeof username);
+console.log(email, typeof email);
+console.log(age, typeof age);
+console.log(isPremium, typeof isPremium);
+
+
+// task17 variable rules
+
+
+var a = 10;
+a = 20;       // Allowed
+var a = 30;   // Allowed
+
+
+let b = 10;
+b = 20;       // Allowed
+ let b = 30;   // Error
+
+const c = 10;
+ c = 20;       // Error
+ const c = 30; // Error
+
+
+//  task18 company database 
+
+let tamil = 85;
+let english = 90;
+let maths = 95;
+let total = tamil + english + maths;
+let average = total / 3;
+console.log("Total:", total);
+console.log("Average:", average);
+
+// task19 company database
+
+let company = {
+    Company: "LOG COMPANY",
+    Employees: ["Thirupathi", "Arun", "Kumar"],
+    Location: "Chennai",
+    FoundedYear: 2020,
+    CEO: "AIJTH KUMAR"
+};
+console.log("CEO:", company.CEO);
+console.log("Location:", company.Location);
+console.log("First Employee:", company.Employees[0]);
+
+// task20 mini profile
+
+let profile = {
+    name: "Thirupathi",
+    city: "Chennai",
+    hobbies: ["Coding", "Cricket", "Music"],
+    age: 22
+};
+console.log("Name:", profile.name);
+console.log("Second Hobby:", profile.hobbies[1]);
+console.log("Age:", profile.age);
